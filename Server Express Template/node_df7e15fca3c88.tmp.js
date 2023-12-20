c9f3a10730d33a59441a295c63bc45bfdc6d0ef7
Yous <Eljilaliyousra@gmail.com>
2023-12-20 12:32:13 +0100

    htdocs - prep

---
+++ Server Express Template/node_df7e15fca3c88.tmp.js
@@ -0,0 +1,14 @@
+// Simple serveur Web avec Express
+// ^ On fait appel à un module Express avec la fonction require
+const express = require('express');
+
+// ^ Ici on crée une instance d'Express appelée app. Elle sera utilisée pour configurer et exécuter votre application
+const app = express();
+
+app.use('/', express.static(__dirname + '/htdocs'));
+
+// ^Cette partie configure le serveur pour écouter les connexions sur le port 8080. Lorsque le serveur démarre, il affiche le message "Listeing ..." dans l'output'
+app.listen(8080, function () {
+    console.log('Listening on port 8080');
+});
+
